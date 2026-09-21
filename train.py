3545861121eeb02ef39c003532d2acbdc6e96aae
UlugbekSalaev <ulugbek0302@gmail.com>
2026-09-21 18:36:39 +0500

    Hotfix 2: Purged dangling benchmark module imports throwing runtime exceptions

--- train.py
+++ train.py
@@ -26,12 +26,10 @@ from data.preprocess import run_preprocessing_pipeline
 from data.dataset import MorphDataset, collate_fn, ALL_TASKS
 from model.model import UzbekMorphModel
 from evaluate import main_evaluate, evaluate_test_set, save_evaluation_results
-from benchmark_uzmorph import run_benchmark, simulate_uzmorph_analyser
 from evaluate_visuals import (
     plot_training_curves,
     plot_per_feature_f1,
     plot_gating_distribution,
-    plot_benchmark_comparison
 )
 
 
